d8c6137dd98fa5a29d938ba724e3aa5f0ef9ca0f
ldiaz94 <ldiaz9@outlook.com>
2019-10-19 21:58:24 +0100

    Remove clutter

--- GAv0.1/Source.cpp
+++ GAv0.1/Source.cpp
@@ -4,18 +4,9 @@
 #define PRINT(thing) std::cout << thing << "\n";
 
 
-enum GA_VALTYPE
+void combine(const void *a, const void *b, void *c)
 {
-	GA_INT = 0,
-	GA_FLOAT32 = 1
-};
-
-
-
-
-void combine(void* a, void* b, void* c, GA_VALTYPE kind)
-{
-	// Cast the void pointers into unsigned int to be able to do the combination easier
+	// Cast the pointers into unsigned int to be able to add to access next byte in memory
 	unsigned int num1 = (unsigned int)a;
 	unsigned int num2 = (unsigned int)b;
 	unsigned int comb = (unsigned int)c;
@@ -32,17 +23,28 @@ void combine(void* a, void* b, void* c, GA_VALTYPE kind)
 
 int main()
 {
+	// Create two ints in hex form. Int comb represents the combination
+	// of "a" and "b" by alternating their byte sequences
+	// Comb therefore is the first byte of "a", the second byte of "b",...
 	int a = 0x003f1dff;
 	int b = 0x010201dd;
 	int comb = 0x013f01ff;
+	PRINT("The value of a:");
 	PRINT(a);
+
+	PRINT("The value of b:");
 	PRINT(b);
+
+	PRINT("Manual combination of a and b:");
 	PRINT(comb);
+
+	// Reset the value of comb, to insure that the function works properly
 	comb = 0x00000000;
-	combine( &a, &b, &comb, GA_INT);
-	PRINT(a);
-	PRINT(b);
+
+	combine( (const void *)&a, (const void *)&b, &comb);
+	PRINT("Using the combination function:");
 	PRINT(comb);
+
 	std::cin.get();
 	return 0;
 }
